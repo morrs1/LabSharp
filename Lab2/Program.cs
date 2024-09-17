dc7f507b2226eedb8a40a6c56b2ca0c5b4e23cb7
morrss <grishkovea@gmail.com>
2024-09-17 18:58:03 +0300

    сделал доп задание 2 лабы по шарпам

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-var motherboard = new Motherboard("ASUS ROG Strix");
+var motherboard = new Motherboard("ASUS ROG Strix", 1660);
 var processor = new Processor("Intel Core i7");
 var hardDisk = new HardDisk("Seagate 1TB");
 var computer = new Computer("Gaming PC", motherboard, processor, hardDisk);
@@ -10,17 +10,20 @@ var keyboard = new Keyboard("Logitech K120");
 var audioSpeakers = new AudioSpeakers("JBL Flip 5");
 
 // Демонстрация работы классов
-List<ComputerEquipment> equipmentList = new List<ComputerEquipment>
-        {
-            computer,
-            printer,
-            monitor,
-            keyboard,
-            audioSpeakers
-        };
+//List<ComputerEquipment> equipmentList = new List<ComputerEquipment>
+//        {
+//            computer,
+//            printer,
+//            monitor,
+//            keyboard,
+//            audioSpeakers
+//        };
 
-foreach (var equipment in equipmentList)
-{
-    equipment.DisplayInfo();
-    Console.WriteLine();
-}
+//foreach (var equipment in equipmentList)
+//{
+//    equipment.DisplayInfo();
+//    Console.WriteLine();
+//}
+
+var motherBoard = new Motherboard("sss", 1660);
+Vanamas.check(motherBoard);
