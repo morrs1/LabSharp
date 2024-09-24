901b396497ddcd7637c5d62a3d8f6ad304040dce
morrs <grishkovea@gmail.com>
2024-09-24 13:44:14 +0300

    добавил класс для проверки совпестимости компонентов и доделал 3 лабу

---
+++ Lab3/ComputerSystem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Lab3.Interfaces;
+
+namespace Lab3
+{
+    internal class ComputerSystem
+    {
+        private List<object> components = [];
+
+        public void AddComponent(object component)
+        {
+            components.Add(component);
+        }
+
+        public void ConnectComponents()
+        {
+            foreach (var component1 in components)
+            {
+                foreach (var component2 in components)
+                {
+                    if (component1 != component2)
+                    {
+                        if (component1 is IUsbBus usbBus1 && component2 is IUsbBus usbBus2)
+                        {
+                            Console.WriteLine($"Соединяю компоненты по шине USB с максимальной скоростью передачи данных: {Math.Min(usbBus1.MaxTransferSpeedUsb, usbBus2.MaxTransferSpeedUsb)}");
+                        }
+                        if (component1 is ISata sata1 && component2 is ISata sata2)
+                        {
+                            Console.WriteLine($"Соединяю компоненты по шине SATA с максимальной скоростью передачи данных: {Math.Min(sata1.MaxTransferSpeedSata, sata2.MaxTransferSpeedSata)}");
+                        }
+                        if (component1 is INetWork network1 && component2 is INetWork network2)
+                        {
+                            Console.WriteLine($"Соединяю компоненты по сети с максимальной скоростью передачи данных: {Math.Min(network1.MaxTransferSpeedNetWork, network2.MaxTransferSpeedNetWork)}");
+                        }
+                        if (component1 is IInnerBus innerBus1 && component2 is IInnerBus innerBus2)
+                        {
+                            Console.WriteLine($"Соединяю компоненты по внутренней шине с максимальной скоростью передачи данных: {Math.Min(innerBus1.MaxTransferSpeedInnerBus, innerBus2.MaxTransferSpeedInnerBus)}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Lab3;
 using Lab3.Classes;
 using Lab3.Interfaces;
 
@@ -6,3 +7,49 @@ Console.WriteLine("Hello, World!");
 var scanner = new Scanner();
 
 scanner.TransferDataNetWork();
+scanner.TransferDataUsb();
+
+var printer = new Printer();
+
+printer.TransferDataNetWork();
+printer.TransferDataUsb();
+
+var ramMemory = new RamMemory();
+
+ramMemory.TransferDataInner();
+
+var networkCard = new NetworkCard();
+
+networkCard.TransferDataNetWork();
+
+var motherBoard = new MotherBoard();
+
+motherBoard.TransferDataNetWork();
+motherBoard.TransferDataUsb();
+motherBoard.TransferDataInner();
+motherBoard.TransferDataSata();
+
+var keyboard = new KeyBoard();
+
+keyboard.TransferDataUsb();
+
+var hardDisk = new HardDisk();
+
+hardDisk.TransferDataSata();
+
+Console.WriteLine();
+
+ComputerSystem computerSystem = new ComputerSystem();
+
+
+computerSystem.AddComponent(motherBoard);
+computerSystem.AddComponent(ramMemory);
+computerSystem.AddComponent(hardDisk);
+computerSystem.AddComponent(printer);
+computerSystem.AddComponent(scanner);
+computerSystem.AddComponent(networkCard);
+computerSystem.AddComponent(keyboard);
+
+computerSystem.ConnectComponents();
+
+
