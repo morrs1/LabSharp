d93ba6ea3c36a610d48f7c4b66b45864f69f6837
morrs <grishkovea@gmail.com>
2024-09-24 13:31:54 +0300

    начал делать 3 лабу

---
+++ Lab3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab3.Classes;
+using Lab3.Interfaces;
+
+Console.WriteLine("Hello, World!");
+var scanner = new Scanner();
+
+scanner.TransferDataNetWork();
