7c0eaa5740a25898d402a679838ae2c0f6a22732
morrs <grishkovea@gmail.com>
2024-11-07 02:56:05 +0300

    feat: добавил класс пользователя для парсинга

---
+++ Lab6/modelsForXml/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab6.modelsForXml;
+
+public class User
+{
+    public string? username { get; set; }
+    public string? password { get; set; }
+
+    public override string ToString()
+    {
+        return $"username = {username}, password = {password}";
+    }
+}
