7b0d6ee4869a00cb6e963f439cd8ce94c7f93025
morrs <grishkovea@gmail.com>
2024-11-07 02:57:31 +0300

    feat: добавил парсер для файла пользователей

--- Lab6/XmlParser/XmlParser.cs
+++ Lab6/XmlParser/XmlParser.cs
@@ -1,6 +1,27 @@
-﻿namespace Lab6;
+﻿using System.Xml.Serialization;
+using Lab6.modelsForXml;
+
+namespace Lab6.XmlParser;
 
 public class XmlParser
 {
+    private const string FILE_PATH = @"C:\Users\grish\RiderProjects\LabSharp\Lab6\xmls\Users.xml";
+    private XmlSerializer _xmlSerializer = new(typeof(Users));
+
+    public void serialize(Users users)
+    {
+        using (var fileStream = new FileStream(FILE_PATH, FileMode.Create))
+        {
+            _xmlSerializer.Serialize(fileStream, users);
+        }
+    }
+
     
+    public Users? deserialize()
+    {
+        using (var fileStream = new FileStream(FILE_PATH, FileMode.Open))
+        {
+            return (Users?)_xmlSerializer.Deserialize(fileStream);
+        }
+    }
 }
