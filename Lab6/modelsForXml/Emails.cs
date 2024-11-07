3e7084b7e1b6bb09276a65e8e5ad0987bc122735
morrss <grishkovea@gmail.com>
2024-11-07 14:47:23 +0300

    feat: добавил класс для сущности списка email

---
+++ Lab6/modelsForXml/Emails.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml.Serialization;
+
+namespace Lab6.modelsForXml;
+
+[XmlRoot("Emails")]
+public class Emails
+{
+    [XmlElement("Email")]
+    public List<Email>? emailsList { get; set; }
+}
