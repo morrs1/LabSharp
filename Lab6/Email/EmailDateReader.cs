d682a46e09b121f8ffcaefdd1290aab51c4bf414
morrss <grishkovea@gmail.com>
2024-11-07 18:02:37 +0300

    feat: добавил класс для вывода информации о разнице во времени между отправкой и текущим моментом

---
+++ Lab6/Email/EmailDateReader.cs
@@ -0,0 +1,12 @@
+﻿using Lab6.XmlParser;
+
+namespace Lab6.Email;
+
+public class EmailDateReader
+{
+    public static void readEmailsWithDate()
+    {
+        var parser = new XmlParserEmails();
+        parser.deserialize().emailsList.ForEach(email => Console.WriteLine(DateTime.Now - email.dateTime));
+    }
+}
