eb3acaf1a99f33eee74d0e63dc9acd3632e9327c
morrss <grishkovea@gmail.com>
2024-11-07 14:46:46 +0300

    feat: добавил класс для сущности email

---
+++ Lab6/modelsForXml/Email.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab6.modelsForXml;
+
+public class Email
+{
+    public string? from { get; set; }
+    public string? to { get; set; }
+    public string? message { get; set; }
+
+    public override string ToString()
+    {
+        return $"from = {from}, to = {to}, message = {message}";
+    }
+}
