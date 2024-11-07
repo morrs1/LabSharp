0b0f816c969c033f0ac407913eb844003e93a5c6
morrss <grishkovea@gmail.com>
2024-11-07 16:57:25 +0300

    feat: добавил класс для отправки сообщений

---
+++ Lab6/Email/EmailSender.cs
@@ -0,0 +1,31 @@
+﻿using Lab6.auth;
+using Lab6.XmlParser;
+
+namespace Lab6.Email;
+
+public class EmailSender
+{
+    private static XmlParser.XmlParser userParser = new XmlParser.XmlParser();
+    private static XmlParserEmails emailParser = new XmlParserEmails();
+    
+    public static string sendEmail(string to, string message)
+    {
+        if (userParser.deserialize().usersList.Find(user => user.username == to) != null)
+        {
+            var from = CurrentUser.currentUsername;
+            var newEmail = new modelsForXml.Email
+            {
+                from = from,
+                to = to,
+                message = message
+            };
+            var emails = emailParser.deserialize();
+            emails?.emailsList?.Add(newEmail);
+            if (emails != null) emailParser.serialize(emails);
+            return "Сообщение отправилось успешно"; 
+        }
+
+        return "Такого пользователя не существует";
+
+    }
+}
