60d81ed8d4c700dd472791cf0bfbda74db729396
morrss <grishkovea@gmail.com>
2024-11-07 16:56:36 +0300

    feat: добавил класс для чтения сообщений

---
+++ Lab6/Email/EmailReader.cs
@@ -0,0 +1,16 @@
+﻿using Lab6.auth;
+using Lab6.XmlParser;
+
+namespace Lab6.Email;
+
+public class EmailReader
+{
+    private static XmlParser.XmlParser userParser = new XmlParser.XmlParser();
+    private static XmlParserEmails emailParser = new XmlParserEmails();
+    
+    public static List<modelsForXml.Email>? readEmail()
+    {
+        return emailParser.deserialize()?.emailsList?.FindAll(email => email.to == CurrentUser.currentUsername);
+
+    }
+}
