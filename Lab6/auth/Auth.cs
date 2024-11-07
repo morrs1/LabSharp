420afdd4657d3f8cfa6c0451c9df73d6840b670f
morrss <grishkovea@gmail.com>
2024-11-07 15:22:34 +0300

    feat: добавил класс аутентификации

---
+++ Lab6/auth/Auth.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab6.auth;
+
+public class Auth
+{
+    private static XmlParser.XmlParser parser = new XmlParser.XmlParser();
+
+    public static string login(string username, string password)
+    {
+        if (parser.deserialize()?.usersList?.Find(user => user.username == username && user.password == password) ==
+            null) return $"Неуспешная аутентификация: неверные данные {username} {password}";
+        CurrentUser.currentUsername = username;
+        return $"Успешная аутентификация под пользователем: {username} {password}";
+
+    }
+}
